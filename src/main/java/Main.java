172c840ed48b031c262ca7e187bfe316024bc52e
Alex <kpecmuk@gmail.com>
2017-02-12 16:33:07 +0300

    time added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,14 +7,20 @@ public class Main {
     private static final int NUMBER = 46;
 
     public static void main(String[] args) {
-        System.out.println("Start...");
+        final long startTime = System.currentTimeMillis();
+        System.out.println("Start..." + (0L) + " ms");
 
         SlowFib slowFib = new SlowFib(NUMBER);
         System.out.println("Fibon " + NUMBER + " = " + slowFib.calculate(slowFib.getNumber()));
 
+        final long middleTime = System.currentTimeMillis();
+        System.out.println("Middle time: " + (middleTime - startTime) + " ms for slowFib");
+
         FastFib fastFib = new FastFib(NUMBER);
         System.out.println("Fibon " + NUMBER + " = " + fastFib.calculate(fastFib.getNumber()));
 
-        System.out.println("End...");
+        final long endTime = System.currentTimeMillis();
+
+        System.out.println("End..." + (endTime - middleTime) + " ms for fastFib");
     }
 }
